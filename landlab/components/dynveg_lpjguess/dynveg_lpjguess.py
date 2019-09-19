2ac082817e789276ed2662492067025dbc0825a0
willi <willi.kappler@uni-tuebingen.de>
2019-09-19 15:47:34 +0200

    Force lpj restart to be zero

--- landlab/components/dynveg_lpjguess/dynveg_lpjguess.py
+++ landlab/components/dynveg_lpjguess/dynveg_lpjguess.py
@@ -164,8 +164,8 @@ def prepare_runfiles(self, step_counter:int, ins_file:str, input_name:str) -> No
     """Prepare files specific to this dt run"""
     # fill template files with per-run data:
     log.warn('REPEATING SPINUP FOR EACH DT !!!')
-    restart = '0' if step_counter == 0 else '1'
-    #restart='0'
+    #restart = '0' if step_counter == 0 else '1'
+    restart = '0'
 
     run_data = {# climate data
                 'CLIMPREC': str(input_name) + '_prec_%s.nc' % str(int(step_counter)).zfill(6),
