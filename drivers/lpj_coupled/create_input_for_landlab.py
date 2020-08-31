ca28738b73508f5745c835e31d0f66aad4964a04
Willi Kappler <willi.kappler@uni-tuebingen.de>
2020-08-31 17:05:47 +0200

    Finish import_vegetation

--- drivers/lpj_coupled/create_input_for_landlab.py
+++ drivers/lpj_coupled/create_input_for_landlab.py
@@ -22,79 +22,7 @@ def _calc_fpc(lai):
     """
     return (1.0 - np.exp(-0.5 * lai)) * 100
 
-
-def _read_csv_files(filename, ftype='lai', pft_class='total'):
-    """
-    reads in the out files from lpj and convertes to aggregated values
-    
-    sp_lai.out
-
-    resulting 2D-arrays have format:
-        
-        tree_fpc[0] = landform__ID
-        tree_fpc[1] = according vegetation cover
-    """
-    month_cols = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec".split(',')
-
-    if ftype == 'lai':
-        index_cols = ['Lat', 'Lon', 'Year', 'Stand', 'Patch'] 
-    elif ftype == 'mprec':
-        index_cols = ['Lat', 'Lon', 'Year', 'Stand'] 
-    else:
-        raise NotImplementedError
-
-    if ftype == 'lai':
-        # these are custom column names (can be configures in LPJ ins file!)
-        tree_cols = ['TeBE_tm','TeBE_itm','TeBE_itscl','TeBS_itm','TeNE','BBS_itm','BBE_itm']
-        shrub_cols = ['BE_s','TeR_s','TeE_s']
-        grass_cols = ['C3G']
-        total_col = ['Total']
-
-        if pft_class == 'total':
-            requested_cols = total_col
-        elif pft_class == 'grass':
-            requested_cols = grass_cols
-        elif pft_class == 'shrub':
-            requested_cols = shrub_cols
-        elif pft_class == 'tree':
-            requested_cols = tree_cols
-        else:
-            raise NotImplementedError
-
-        try:
-            df = pd.read_table(filename, delim_whitespace=True)[index_cols + requested_cols]
-            df = df[df.Stand > 0]
-            del df['Patch']
-            df_grp = df.groupby(['Lon', 'Lat', 'Year', 'Stand'], sort = False).mean()
-            df_grp = df_grp.apply(_calc_fpc, 1).sum(axis=1)
-            x = df_grp.reset_index().set_index(['Year', 'Stand'])
-
-            del x['Lon'], x['Lat']
-
-            data = x.mean(level=1).T / 100
-        except pd.errors.EmptyDataError:
-            return None
-    
-    elif ftype == 'mprec':
-        try:
-            df = pd.read_table(filename, delim_whitespace=True)[index_cols + month_cols]        
-            df = df[df.Stand > 0]
-            df['Annual'] = df[month_cols].sum(axis=1)
-            for mc in month_cols:
-                del df[mc]
-            x = df.reset_index().set_index(['Year', 'Stand'])
-            del x['index'], x['Lon'], x['Lat']
-
-            data = x.mean(level=1).T / 10
-        except pd.errors.EmptyDataError:
-            return None
-
-    else:
-        raise NotImplementedError    
- 
-    return data.to_records()
-
-def map_fpc_per_landform_on_grid(grid, fpc_array):
+def map_vegi_per_landform_on_grid(grid, vegi_array):
     """
     extract the tree fractional cover per landform
     
@@ -105,14 +33,14 @@ def map_fpc_per_landform_on_grid(grid, fpc_array):
     """
 
     #creates grid_structure for landlab
-    fpc_grid = np.zeros(np.shape(grid.at_node['landform__ID']))
+    vegi_grid = np.zeros(np.shape(grid.at_node['landform__ID']))
     
-    if fpc_array is not None:
-        for landform in fpc_array.dtype.names[1:]:
-            fpc_grid[grid.at_node['landform__ID'] == int(landform)] = fpc_array[str(landform)]
+    if vegi_array is not None:
+        for landform in vegi_array.dtype.names[1:]:
+            vegi_grid[grid.at_node['landform__ID'] == int(landform)] = vegi_array[str(landform)]
 
     #print('map_fpc_per_landform_on_grid was run')
-    return fpc_grid
+    return vegi_grid
 
 def map_precip_per_landform_on_grid(grid, precip_array):
     """
@@ -132,38 +60,74 @@ def map_precip_per_landform_on_grid(grid, precip_array):
 
     return precip_grid
 
-def calc_cumulative_fpc(tree_fpc, grass_fpc, shrub_fpc):
-    """
-    If you want to use total vegetation cover instead of individual cover, this
-    script adds up trees, shrubs, grass
-    """
+def process_vegetation_data(data, index_cols, other_cols):
+    data = data[index_cols + other_cols]
+    data = data.groupby(index_cols, sort = False).mean()
+    fpc_data = data.apply(_calc_fpc, 1).sum(axis=1)
+    fpc_data = fpc_data.reset_index().set_index(index_cols)
+    fpc_data = fpc_data.mean(level=1).T / 100
 
-    total_fpc = tree_fpc[1] + shrub_fpc[1] + grass_fpc[1]
+    lai_data = data.reset_index().set_index(index_cols)
+    lai_data = lai_data.mean(level=1).T / 100
 
-    cumulative_fpc = tree_fpc.copy()
-    cumulative_fpc[1] = total_fpc
-
-
-    return cumulative_fpc
+    return (fpc_data.to_records(), lai_data.to_records())
 
 def import_vegetation(grid, vegi_mapping_method, filename):
     csv_data = pd.read_table(filename, delim_whitespace=True)
-    filtered_data = csv_data[csv_data.Stand > 0]
+    csv_data = csv_data[csv_data.Stand > 0]
+    index_cols = ['Year', 'Stand'] 
 
-    if vegi_mapping_method == 'cumulative':
-        pass
-    elif vegi_mapping_method == 'individual':
+    total_col = ['Total']
+
+    (total_fpc, total_lai) = process_vegetation_data(csv_data, index_cols, total_col)
+
+    if 'vegetation__density' not in grid.keys('node'):
+        grid.add_zeros('node', 'vegetation__density')
+    if 'vegetation__density_lai' not in grid.keys('node'):
+        grid.add_zeros('node', 'vegetation__density_lai')
+
+    grid.at_node['vegetation__density'] = map_vegi_per_landform_on_grid(grid, total_fpc)
+    grid.at_node['vegetation__density_lai'] = map_vegi_per_landform_on_grid(grid, total_lai)
+
+
+    if vegi_mapping_method == 'individual':
         tree_cols = ['TeBE_tm','TeBE_itm','TeBE_itscl','TeBS_itm','TeNE','BBS_itm','BBE_itm']
         shrub_cols = ['BE_s','TeR_s','TeE_s']
         grass_cols = ['C3G']
 
+        (tree_fpc, tree_lai) = process_vegetation_data(csv_data, index_cols, tree_cols)
+        (shrub_fpc, shrub_lai) = process_vegetation_data(csv_data, index_cols, shrub_cols)
+        (grass_fpc, grass_lai) = process_vegetation_data(csv_data, index_cols, grass_cols)
 
+        if 'tree_fpc' not in grid.keys('node'):
+            grid.add_zeros('node', 'tree_fpc')
+        if 'tree_lai' not in grid.keys('node'):
+            grid.add_zeros('node', 'tree_lai')
+
+        grid.at_node['tree_fpc'] = map_vegi_per_landform_on_grid(grid, tree_fpc)
+        grid.at_node['tree_lai'] = map_vegi_per_landform_on_grid(grid, tree_lai)
+
+        if 'shrub_fpc' not in grid.keys('node'):
+            grid.add_zeros('node', 'shrub_fpc')
+        if 'shrub_lai' not in grid.keys('node'):
+            grid.add_zeros('node', 'shrub_lai')
+
+        grid.at_node['shrub_fpc']  = map_vegi_per_landform_on_grid(grid, shrub_fpc)
+        grid.at_node['shrub_lai']  = map_vegi_per_landform_on_grid(grid, shrub_lai)
+
+        if 'grass_fpc' not in grid.keys('node'):
+            grid.add_zeros('node', 'grass_fpc')
+        if 'grass_lai' not in grid.keys('node'):
+            grid.add_zeros('node', 'grass_lai')
+
+        grid.at_node['grass_fpc'] = map_vegi_per_landform_on_grid(grid, grass_fpc)
+        grid.at_node['grass_lai'] = map_vegi_per_landform_on_grid(grid, grass_lai)
 
 def import_precipitation(grid, filename):
     csv_data = pd.read_table(filename, delim_whitespace=True)
 
     month_cols = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec".split(',')
-    index_cols = ["Year","Stand"]
+    index_cols = ["Year", "Stand"]
 
     filtered_data = csv_data[index_cols + month_cols][csv_data.Stand > 0]
     filtered_data["Annual"] = filtered_data[month_cols].sum(axis = 1)
@@ -184,55 +148,3 @@ def lpj_import_run_one_step(grid, vegi_mapping_method):
 
     import_vegetation(grid, vegi_mapping_method, "./temp_lpj/output/sp_lai.out")
     import_precipitation(grid, "./temp_lpj/output/sp_mprec.out")
-
-
-
-
-
-
-def _lpj_import_run_one_step(grid, vegi_mapping_method):
-    
-    if var == 'lai':
-        if method == 'cumulative':
-            cum_fpc = read_csv_files(inputFile, ftype = var, pft_class = 'total')
-            grid.at_node['vegetation__density'] = map_fpc_per_landform_on_grid(grid, cum_fpc)
-
-        elif method == 'individual':
-            #add individual landlab fields to the grid if they don't exist
-            if all(fpc in grid.keys('node') for fpc in ('grass_fpc', 'tree_fpc',
-                'shrub_fpc')):
-                #no need to initialize something. just do nothing
-                pass
-            else:
-                grid.add_zeros('node', 'grass_fpc')
-                grid.add_zeros('node', 'tree_fpc')
-                grid.add_zeros('node', 'shrub_fpc')
-
-            grass_fpc = read_csv_files(inputFile, ftype = 'lai', pft_class = 'grass')
-            tree_fpc  = read_csv_files(inputFile, ftype = 'lai', pft_class = 'tree')
-            shrub_fpc = read_csv_files(inputFile, ftype = 'lai', pft_class = 'shrub')
-            cum_fpc = read_csv_files(inputFile, ftype = var, pft_class = 'total')
-
-            #map values to individual fields
-            grid.at_node['grass_fpc'] = map_fpc_per_landform_on_grid(grid, grass_fpc)
-            grid.at_node['tree_fpc']  = map_fpc_per_landform_on_grid(grid, tree_fpc)
-            grid.at_node['shrub_fpc'] = map_fpc_per_landform_on_grid(grid, shrub_fpc)
-            grid.at_node['vegetation__density'] = map_fpc_per_landform_on_grid(grid, cum_fpc)
-
-    elif var == 'mprec':
-        # Check if precipitation field already exists in grid, if not, initiate it
-        if 'precipitation' not in grid.keys('node'):
-            grid.add_zeros('node', 'precipitation')
-        
-        precip_array = read_csv_files(inputFile, ftype = 'mprec')
-        grid.at_node['precipitation'] = map_precip_per_landform_on_grid(grid, precip_array )
-        #add precipitation to modelgrid
-        #TODO: check if this has spatial variability
-        #grid.at_node['precipitation'][:] = prec
-
-    else: 
-        raise NotImplementedError
-
-
-
-
