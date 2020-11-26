f03a9616fbe15018d3f69b14e8f1c3d1bcae066a
Willi Kappler <willi.kappler@uni-tuebingen.de>
2020-11-26 10:06:03 +0100

    Add one more bin to be on the safe side as recommended by Mirjam Schaller

--- drivers/lpj_coupled/create_all_landforms.py
+++ drivers/lpj_coupled/create_all_landforms.py
@@ -32,7 +32,7 @@ def create_all_landforms(uplift, runtime, elevation_bin_size, grid):
     _max_possible_elevation_gain = uplift * runtime #note: have to be same units
     _max_possible_elevation = _max_possible_elevation_gain + _min_initial_elevation
 
-    _max_possible_ele_id = int(_max_possible_elevation / elevation_bin_size) + 1
+    _max_possible_ele_id = int(_max_possible_elevation / elevation_bin_size) + 2
     _min_possible_ele_id = int(_min_initial_elevation / elevation_bin_size) + 1
 
     logging.debug("_min_initial_elevation: %f", _min_initial_elevation)
