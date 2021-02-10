b7a9a48f29db79df9b37f47b9504e1cd0efb638b
Willi Kappler <willi.kappler.gm@gmail.com>
2021-02-10 17:48:56 +0100

    Add wet days to precipitation

--- drivers/helperScripts/gen_clim_data.py
+++ drivers/helperScripts/gen_clim_data.py
@@ -19,8 +19,7 @@ elif mode == "daily":
 else:
     raise Exception("Unknown mode: {}".format(mode))
 
-def gen_data(file_name_base, lat_val, lon_val, var_name, var_value, var_description,
-             var_long_name, var_units, var_code):
+def gen_common(file_name_base, lat_val, lon_val, var_name):
     file_name = "{}_{}.nc".format(file_name_base, var_name)
     f = netCDF4.Dataset(file_name, "w", format = "NETCDF4") # pylint: disable=no-member
 
@@ -32,7 +31,6 @@ def gen_data(file_name_base, lat_val, lon_val, var_name, var_value, var_descript
     time = f.createVariable("time", num_type, "time")
     lat = f.createVariable("lat", num_type, "land_id")
     lon = f.createVariable("lon", num_type, "land_id")
-    var_instance = f.createVariable(var_name, num_type, ("land_id", "time"))
     land_id = f.createVariable("land_id", "i8", "land_id")
 
     if mode == "monthly":
@@ -47,7 +45,6 @@ def gen_data(file_name_base, lat_val, lon_val, var_name, var_value, var_descript
 
     lat[:] = np.full(1, lat_val)
     lon[:] = np.full(1, lon_val)
-    var_instance[:] = np.full(NUM_OF_ELEMENTS, var_value)
     land_id[:] = np.full(1, 0)
 
     time.axis = "T"
@@ -64,21 +61,38 @@ def gen_data(file_name_base, lat_val, lon_val, var_name, var_value, var_descript
     lon.long_name = "longitude"
     lon.units = "degrees_east"
 
+    return f
+
+def gen_var_data(f, var_name, var_value, var_description,
+             var_long_name, var_units, var_code = None):
+    num_type = "f8"
+
+    var_instance = f.createVariable(var_name, num_type, ("land_id", "time"))
+    var_instance[:] = np.full(NUM_OF_ELEMENTS, var_value)
     var_instance.coordinates = "lat lon"
     var_instance.standard_name = var_description
     var_instance.long_name = var_long_name
     var_instance.units = var_units
-    var_instance.code = var_code
+    if var_code:
+        var_instance.code = var_code
     var_instance.table = "128"
 
+
+def gen_data_for_location(file_name_base, lat, lon, prec, wet, temp, rad):
+    f = gen_common(file_name_base, lat, lon, "prec")
+    gen_var_data(f, "prec", prec, "precipitation_amount", "daily precipitation amount", "kg m-2", "260")
+    gen_var_data(f, "wet", wet, "number_of_days_with_lwe_thickness_of_precipitation_amount_above_threshold", "wet_days", "count")
     f.close()
 
-def gen_data_for_location(file_name_base, lat, lon, prec, temp, rad):
-    gen_data(file_name_base, lat, lon, "prec", prec, "precipitation_amount", "daily precipitation amount", "kg m-2", "260")
-    gen_data(file_name_base, lat, lon, "temp", temp, "air_temperature", "Near surface air temperature at 2m", "K", "167")
-    gen_data(file_name_base, lat, lon, "rad", rad, "surface_downwelling_shortwave_flux", "Mean daily surface incident shortwave radiation", "W m-2", "176")
+    f = gen_common(file_name_base, lat, lon, "temp")
+    gen_var_data(f, "temp", temp, "air_temperature", "Near surface air temperature at 2m", "K", "167")
+    f.close()
 
-gen_data_for_location("LaCampana_LGM", lat = -32.75, lon = -71.25, prec = 0.92, temp = 284.6, rad = 249.1)
+    f = gen_common(file_name_base, lat, lon, "rad")
+    gen_var_data(f, "rad", rad, "surface_downwelling_shortwave_flux", "Mean daily surface incident shortwave radiation", "W m-2", "176")
+    f.close()
+
+gen_data_for_location("LaCampana_LGM", lat = -32.75, lon = -71.25, prec = 0.92, wet = 8.24, temp = 284.6, rad = 249.1)
 
 with open("co2_data.txt", "w+") as f:
     for i in range(1, NUM_OF_ELEMENTS):
