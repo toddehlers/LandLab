ed068dab34c56eeefff3c96d13758e6276d2656d
Willi Kappler <willi.kappler.gm@gmail.com>
2021-03-09 21:39:07 +0100

    Remove elevation_bin_size

--- drivers/lpj_coupled/runfile_space.py
+++ drivers/lpj_coupled/runfile_space.py
@@ -317,13 +317,13 @@ while elapsed_time < totalT:
     DDdiff.run_one_step(dt=dt)
 
     #run the landform classifier
-    lc.run_one_step(elevationStepBin, 300, classificationType, max_elevation)
+    lc.run_one_step(300, classificationType, max_elevation)
 
     #run lpjguess once at the beginning and then each timestep after the spinup.
     if elapsed_time < spin_up:
         if elapsed_time == 0:
             #create all possible landform__ID's in here ONCE before lpjguess is called
-            max_elevation = create_all_landforms(upliftRate, totalT, elevationStepBin, mg)
+            max_elevation = create_all_landforms(upliftRate, totalT, mg)
             # Output file needs to be written before lpj is started
             # Currently some information from the output is needed as preparation
             # for LPJGuess.
