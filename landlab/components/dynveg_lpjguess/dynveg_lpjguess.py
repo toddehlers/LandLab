5a283dd69c5c8f66677ed0bb610ec3539a9d68df
Willi Kappler <willi.kappler.gm@gmail.com>
2021-03-15 17:24:16 +0100

    Remove unused code

--- landlab/components/dynveg_lpjguess/dynveg_lpjguess.py
+++ landlab/components/dynveg_lpjguess/dynveg_lpjguess.py
@@ -76,11 +76,11 @@ def split_climate(time_step,
             for g_cnt, ds_grp in tqdm(ds.groupby(ds.grouper)):
                 del ds_grp['grouper']
 
-                if g_cnt == 0:
-                    if time_step == "monthly":
-                        ds_grp['time'][:dt*12]
-                    else:
-                        ds_grp['time'][:dt*365]
+#                if g_cnt == 0:
+#                    if time_step == "monthly":
+#                        ds_grp['time'][:dt*12]
+#                    else:
+#                        ds_grp['time'][:dt*365]
 
                 add_time_attrs(ds_grp, calendar_year)
                 foutname = os.path.basename(fpath.replace('.nc', ''))
