f0303ebd75531bb84f0738a965d59b648d521b7e
Willi Kappler <willi.kappler.gm@gmail.com>
2021-04-08 18:05:13 +0200

    Add export function

--- drivers/helperScripts/repeat_100yr.py
+++ drivers/helperScripts/repeat_100yr.py
@@ -111,17 +111,63 @@ def extract_and_repeat(file_prefix, first_day, last_day, count):
     rad_data = list(itertools.islice(rad_data, number_of_elements_new))
 
     export_data("{}_repeat_prec.nc".format(file_prefix), days, longitude, latitude, land_id,
-    [(prec_data, "precipitation_amount", "Monthly precipitation amount", "kg m-2"),
-     (wet_data, "number_of_days_with_lwe_thickness_of_precipitation_amount_above_threshold", "wet_days", "count")])
+    [(prec_data, "prec", "precipitation_amount", "Monthly precipitation amount", "kg m-2"),
+     (wet_data, "wet", "number_of_days_with_lwe_thickness_of_precipitation_amount_above_threshold", "wet_days", "count")])
 
     export_data("{}_repeat_temp.nc".format(file_prefix), days, longitude, latitude, land_id,
-    [(temp_data, "air_temperature", "Near surface air temperature at 2m", "K")])
+    [(temp_data, "temp", "air_temperature", "Near surface air temperature at 2m", "K")])
 
     export_data("{}_repeat_rad.nc".format(file_prefix), days, longitude, latitude, land_id,
-    [(rad_data, "surface_downwelling_shortwave_flux", "Mean daily surface incident shortwave radiation", "W m-2")])
+    [(rad_data, "rad", "surface_downwelling_shortwave_flux", "Mean daily surface incident shortwave radiation", "W m-2")])
 
 def export_data(filename, days, longitude, latitude, land_id, data):
-    pass
+    netcdf_out_ds = netCDF4.Dataset(filename, "w", format = "NETCDF4") # pylint: disable=no-member
+
+    netcdf_out_ds.createDimension("time", days.len())
+    netcdf_out_ds.createDimension("land_id", 1)
+
+    num_type = "f8"
+
+    time = netcdf_out_ds.createVariable("time", num_type, "time")
+    lat = netcdf_out_ds.createVariable("lat", num_type, "land_id")
+    lon = netcdf_out_ds.createVariable("lon", num_type, "land_id")
+    land_id = netcdf_out_ds.createVariable("land_id", "i8", "land_id")
+
+    time[:] = np.array(days)
+
+    lat[:] = np.full(1, latitude)
+    lon[:] = np.full(1, longitude)
+    land_id[:] = np.full(1, 0)
+
+    num_of_elements = days / 12
+
+    time.axis = "T"
+    time.standard_name = "time"
+    time.long_name = "time"
+    time.units = "day"
+    time.calendar = "{} yr B.P.".format(num_of_elements)
+
+    lat.standard_name = "latitude"
+    lat.long_name = "latitude"
+    lat.units = "degrees_north"
+
+    lon.standard_name = "longitude"
+    lon.long_name = "longitude"
+    lon.units = "degrees_east"
+
+    for d in data:
+        var_value = d[0]
+        var_name = d[1]
+        var_description = d[2]
+        var_long_name = d[3]
+        var_units = d[4]
+
+        var_instance = netcdf_out_ds.createVariable(var_name, num_type, ("land_id", "time"))
+        var_instance[:] = np.full(num_of_elements, var_value)
+        var_instance.coordinates = "lon lat"
+        var_instance.standard_name = var_description
+        var_instance.long_name = var_long_name
+        var_instance.units = var_units
 
 if __name__ == "__main__":
-    extract_and_repeat("", 0.0, YEARS_100, 220)
+    extract_and_repeat("Nahuelbuta_TraCE21ka", 0.0, YEARS_100, 220)
