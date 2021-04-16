c7e1c5a3904f69b1e4ad210ccbfd19c820215d0e
Willi Kappler <willi.kappler.gm@gmail.com>
2021-04-16 15:13:34 +0200

    First commit of new shell script

---
+++ drivers/helperScripts/repeat_100yr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for f in LaCampana_TraCE21ka_*.nc; do
+    echo $f;
+    ncap2 -O -s 'time@calendar="365_day";
+    t[time]=0;
+    @all=get_vars_in();
+    for (vi = 0; vi < @all.size(); vi++) {
+        @var_nm = @all(vi);
+        if (@var_nm.ndims() != 2) continue;
+        print(@var_nm, "%s\n");
+        t = 0;
+        for (idx = 0; idx < $time.size; idx++) {
+            t(idx) = @var_nm(0, idx % (100*12));
+        };
+        @var_nm(0,:) = t;
+    }' "$f" tmp.nc;
+    ncpdq -a time,land_id tmp.nc ${f/.nc}_first_100a_repeating.nc;
+done
