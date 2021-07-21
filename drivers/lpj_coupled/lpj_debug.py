396f61f6c0de457b62976e77cdab4d4c817c9cfa
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-07-21 11:48:34 +0200

    Try dir_util.copy_tree instead of shutil.copytree

--- drivers/lpj_coupled/lpj_debug.py
+++ drivers/lpj_coupled/lpj_debug.py
@@ -1,9 +1,11 @@
 
-import shutil
+# import shutil
+import distutils.dir_util as du
 
 class LPJDebug:
     def __init__(self, temp_folder):
         self.temp_folder = temp_folder
     
     def copy_temp_lpj(self, elapsed_time):
-        shutil.copytree(self.temp_folder, "debugging/{}__{}".format(self.temp_folder, elapsed_time))
+        # shutil.copytree(self.temp_folder, "debugging/{}__{}".format(self.temp_folder, elapsed_time))
+        du.copy_tree(self.temp_folder, "debugging/{}__{}".format(self.temp_folder, elapsed_time))
